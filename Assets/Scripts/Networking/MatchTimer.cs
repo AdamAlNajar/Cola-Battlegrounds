1cb69636482438ae41f409fced40c5531115a08e
Adam Al Najar <119093751+AdamAlNajar@users.noreply.github.com>
2025-06-16 09:42:39 +0400

    Update MatchTimer.cs

--- Assets/Scripts/Networking/MatchTimer.cs
+++ Assets/Scripts/Networking/MatchTimer.cs
@@ -73,6 +73,7 @@ public class MatchTimer : MonoBehaviourPunCallbacks,IPunObservable
         Cursor.visible = true;
     }
     //This method is better for syncing frequent updates to not flood photons servers
+    // EG of Client Authorative state replication
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting && PhotonNetwork.IsMasterClient)
